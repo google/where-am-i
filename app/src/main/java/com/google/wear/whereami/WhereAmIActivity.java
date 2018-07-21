15fd54bd3bc06951bee2e19f3d3b7f388a8bc13b
Tyler Freeman <fuego@google.com>
2018-07-20 17:05:04 -0700

    Initial commit

---
+++ app/src/main/java/com/google/wear/whereami/WhereAmIActivity.java
@@ -0,0 +1,94 @@
+package com.google.wear.whereami;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Address;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.wearable.activity.WearableActivity;
+import android.text.format.DateUtils;
+import android.util.Pair;
+import android.widget.TextView;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.wear.whereami.complication.WhereAmIComplicationProviderService;
+import com.patloew.rxlocation.RxLocation;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+
+public class WhereAmIActivity extends FragmentActivity {
+
+    private final CompositeDisposable subscriptions = new CompositeDisposable();
+
+    private TextView mTextView;
+    private RxLocation rxLocation;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.where_am_i_activity);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+//        setAmbientEnabled();
+
+        rxLocation = new RxLocation(this);
+
+        subscriptions.add(
+            checkPermissions()
+                .flatMap((isGranted) -> rxLocation.location().updates(createLocationRequest()))
+                .flatMapMaybe((location ->
+                    rxLocation.geocoding()
+                            .fromLocation(location)
+                            .map(address -> Pair.create(location, address))
+//                            .toSingle()
+                ))
+                .subscribe(
+                    // onNext
+                    (locationAndAddress) -> {
+                    mTextView.setText(getString(
+                            R.string.address_as_of_time_ago,
+                            WhereAmIComplicationProviderService.getAddressDescription(this, locationAndAddress.second),
+                            getTimeAgo(locationAndAddress.first.getTime())));
+                    },
+                    // onError
+                    (error) -> mTextView.setText(R.string.location_error)
+                )
+        );
+    }
+
+    @Override
+    public void onDestroy() {
+        subscriptions.dispose();
+        super.onDestroy();
+    }
+
+    private Observable<Boolean> checkPermissions() {
+        return new RxPermissions(this)
+                .request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .map(isGranted -> {
+                    if (isGranted) return true;
+                    throw new SecurityException("No location permission");
+                });
+    }
+
+    private CharSequence getTimeAgo(long time) {
+        return DateUtils.getRelativeTimeSpanString(time);
+    }
+
+
+    private static LocationRequest createLocationRequest() {
+        return LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+//                .setExpirationDuration(TimeUnit.SECONDS.toMillis(130))
+                .setInterval(TimeUnit.SECONDS.toMillis(10))
+                .setSmallestDisplacement(50);
+    }
+}
---
+++ app/src/main/java/com/google/wear/whereami/complication/WhereAmIComplicationProviderService.java
@@ -0,0 +1,153 @@
+package com.google.wear.whereami.complication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.support.wearable.complications.ComplicationData;
+import android.support.wearable.complications.ComplicationManager;
+import android.support.wearable.complications.ComplicationProviderService;
+import android.support.wearable.complications.ComplicationText;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.wear.whereami.R;
+import com.google.wear.whereami.WhereAmIActivity;
+import com.patloew.rxlocation.FusedLocation;
+import com.patloew.rxlocation.RxLocation;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+public class WhereAmIComplicationProviderService extends ComplicationProviderService {
+
+    private static final String TAG = "WhereAmIComplication";
+
+    private RxLocation rxLocation;
+    private final CompositeDisposable subscriptions = new CompositeDisposable();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        rxLocation = new RxLocation(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        subscriptions.dispose();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onComplicationUpdate(int complicationId, int dataType, ComplicationManager manager) {
+
+        Log.d(TAG, "onComplicationUpdate() id: " + complicationId);
+
+        Location location = null;
+        Address address = null;
+        try {
+            if (checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                FusedLocation locationProvider = rxLocation.location();
+                //        subscriptions.add(
+                location = locationProvider
+                        .isLocationAvailable()
+                        .flatMapObservable((hasLocation) -> hasLocation ?
+                                locationProvider.lastLocation().toObservable() :
+                                locationProvider.updates(createLocationRequest()))
+                        .blockingFirst();
+
+                address = rxLocation.geocoding()
+                        .fromLocation(location)
+                        .blockingGet();
+            } else {
+                Log.w(TAG, "No location permission!");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to look up address", e);
+        }
+
+        Log.d(TAG, "Address: " + address);
+
+        ComplicationData complicationData = null;
+        switch (dataType) {
+            case ComplicationData.TYPE_SHORT_TEXT:
+                complicationData =
+                        new ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
+                                .setShortText(getTimeAgo(location))
+                                .setContentDescription(getFullDescription(location, address))
+                                .setTapAction(getTapAction())
+                                .build();
+                break;
+            case ComplicationData.TYPE_LONG_TEXT:
+                complicationData =
+                        new ComplicationData.Builder(ComplicationData.TYPE_LONG_TEXT)
+                                .setLongTitle(getTimeAgo(location))
+                                .setLongText(getAddressDescription(this, address))
+                                .setContentDescription(getFullDescription(location, address))
+                                .setTapAction(getTapAction())
+                                .build();
+                break;
+            default:
+                if (Log.isLoggable(TAG, Log.WARN)) {
+                    Log.w(TAG, "Unexpected complication type " + dataType);
+                }
+        }
+
+        if (complicationData != null) {
+            manager.updateComplicationData(complicationId, complicationData);
+        } else {
+            // If no data is sent, we still need to inform the ComplicationManager, so
+            // the update job can finish and the wake lock isn't held any longer.
+            manager.noUpdateRequired(complicationId);
+        }
+    }
+
+
+    private PendingIntent getTapAction() {
+        Intent intent = new Intent(this, WhereAmIActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private ComplicationText getFullDescription(Location location, Address address) {
+        if (location == null || address == null) return ComplicationText.plainText(getString(R.string.no_location));
+
+        return getTimeAgo(location.getTime())
+                .setSurroundingText(getString(R.string.address_as_of_time_ago, getAddressDescription(this, address), "^1"))
+                .build();
+    }
+
+    public static ComplicationText getAddressDescription(Context context, Address address) {
+        if (address == null) return  ComplicationText.plainText(context.getString(R.string.no_location));
+        String subThoroughfare = address.getSubThoroughfare();
+        return ComplicationText.plainText(address.getThoroughfare() +
+                (TextUtils.isEmpty(subThoroughfare) ? "" : ": " + subThoroughfare));
+    }
+
+    private ComplicationText getTimeAgo(Location location) {
+        if (location == null) return ComplicationText.plainText("--");
+        return getTimeAgo(location.getTime()).build();
+    }
+
+    private ComplicationText.TimeDifferenceBuilder getTimeAgo(long fromTime) {
+        return new ComplicationText.TimeDifferenceBuilder()
+                .setStyle(ComplicationText.DIFFERENCE_STYLE_SHORT_WORDS_SINGLE_UNIT)
+                .setMinimumUnit(TimeUnit.MINUTES)
+                .setReferencePeriodEnd(fromTime)
+                .setShowNowText(true);
+    }
+
+    public static LocationRequest createLocationRequest() {
+        return LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                .setExpirationDuration(TimeUnit.SECONDS.toMillis(30));
+    }
+}
