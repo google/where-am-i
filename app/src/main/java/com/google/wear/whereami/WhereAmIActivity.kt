2189cb617cf4e699cdd4fe5a6a77e9d53c6759b3
Yuri Schimke <yuri@schimke.ee>
2021-08-17 15:35:43 +0100

    Kotlin switch

--- app/src/main/java/com/google/wear/whereami/WhereAmIActivity.kt
+++ app/src/main/java/com/google/wear/whereami/WhereAmIActivity.kt
@@ -11,110 +11,107 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package com.google.wear.whereami
 
-package com.google.wear.whereami;
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Location
+import android.os.Bundle
+import android.text.format.DateUtils
+import android.util.Pair
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.wear.complications.datasource.ComplicationDataSourceUpdateRequester
+import com.google.android.gms.location.LocationRequest
+import com.google.wear.whereami.complication.WhereAmIComplicationProviderService.Companion.getAddressDescription
+import com.patloew.rxlocation.RxLocation
+import com.tbruyelle.rxpermissions2.RxPermissions
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import androidx.fragment.app.FragmentActivity;
-import android.text.format.DateUtils;
-import android.util.Pair;
-import android.widget.TextView;
-import androidx.wear.complications.datasource.ComplicationDataSourceUpdateRequester;
-
-import com.google.android.gms.location.LocationRequest;
-import com.google.wear.whereami.complication.WhereAmIComplicationProviderService;
-import com.patloew.rxlocation.RxLocation;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
-
-public class WhereAmIActivity extends FragmentActivity {
-
-    private final CompositeDisposable subscriptions = new CompositeDisposable();
-
-    private TextView textView;
-    private RxLocation rxLocation;
+class WhereAmIActivity : FragmentActivity() {
+    private val subscriptions = CompositeDisposable()
+    private var textView: TextView? = null
+    private var rxLocation: RxLocation? = null
 
     @SuppressLint("MissingPermission")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.where_am_i_activity);
-
-        textView = (TextView) findViewById(R.id.text);
-
-        rxLocation = new RxLocation(this);
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.where_am_i_activity)
+        textView = findViewById<View>(R.id.text) as TextView
+        rxLocation = RxLocation(this)
         subscriptions.add(
             checkPermissions()
                 .subscribeOn(Schedulers.io())
-                .flatMap((isGranted) -> rxLocation.location().updates(createLocationRequest()))
-                .flatMapMaybe((location ->
-                    rxLocation.geocoding()
-                            .fromLocation(location)
-                            .map(address -> Pair.create(location, address))
-                ))
+                .flatMap {
+                    rxLocation!!.location().updates(createLocationRequest())
+                }
+                .flatMapMaybe { location: Location ->
+                    rxLocation!!.geocoding()
+                        .fromLocation(location)
+                        .map { address: Address -> Pair.create(location, address) }
+                }
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    // onNext
-                    (locationAndAddress) -> {
-                        textView.setText(getString(
+                .subscribe( // onNext
+                    { locationAndAddress: Pair<Location, Address> ->
+                        textView!!.text = getString(
                             R.string.address_as_of_time_activity,
-                            WhereAmIComplicationProviderService.getAddressDescription(this, locationAndAddress.second),
-                            getTimeAgo(locationAndAddress.first.getTime())));
-                    },
-                    // onError
-                    (error) -> textView.setText(R.string.location_error)
-                )
-        );
+                            getAddressDescription(this, locationAndAddress.second),
+                            getTimeAgo(locationAndAddress.first.time)
+                        )
+                    }
+                )  // onError
+                { textView!!.setText(R.string.location_error) }
+        )
     }
 
-    @Override
-    public void onDestroy() {
-        subscriptions.dispose();
-        super.onDestroy();
+    public override fun onDestroy() {
+        subscriptions.dispose()
+        super.onDestroy()
     }
 
-    @Override
-    protected void onStop() {
-        forceComplicationUpdate();
-        super.onStop();
+    override fun onStop() {
+        forceComplicationUpdate()
+        super.onStop()
     }
 
-    private void forceComplicationUpdate() {
+    private fun forceComplicationUpdate() {
         if (checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            ComplicationDataSourceUpdateRequester request =
-                    new ComplicationDataSourceUpdateRequester(getApplicationContext(), ComponentName.createRelative(getApplicationContext(), ".complication.WhereAmIComplicationProviderService"));
-            request.requestUpdateAll();
+            val request = ComplicationDataSourceUpdateRequester(
+                applicationContext, ComponentName.createRelative(
+                    applicationContext, ".complication.WhereAmIComplicationProviderService"
+                )
+            )
+            request.requestUpdateAll()
         }
     }
 
-    private Observable<Boolean> checkPermissions() {
-        return new RxPermissions(this)
-                .request(Manifest.permission.ACCESS_FINE_LOCATION)
-                .map(isGranted -> {
-                    if (isGranted) return true;
-                    throw new SecurityException("No location permission");
-                });
+    private fun checkPermissions(): Observable<Boolean> {
+        return RxPermissions(this)
+            .request(Manifest.permission.ACCESS_FINE_LOCATION)
+            .map { isGranted: Boolean ->
+                if (isGranted) return@map true
+                throw SecurityException("No location permission")
+            }
     }
 
-    private CharSequence getTimeAgo(long time) {
-        return DateUtils.getRelativeTimeSpanString(time);
+    private fun getTimeAgo(time: Long): CharSequence {
+        return DateUtils.getRelativeTimeSpanString(time)
     }
 
-    private static LocationRequest createLocationRequest() {
-        return LocationRequest.create()
+    companion object {
+        private fun createLocationRequest(): LocationRequest {
+            return LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(TimeUnit.SECONDS.toMillis(10))
-                .setSmallestDisplacement(50);
+                .setSmallestDisplacement(50f)
+        }
     }
 }
--- app/src/main/java/com/google/wear/whereami/complication/WhereAmIComplicationProviderService.kt
+++ app/src/main/java/com/google/wear/whereami/complication/WhereAmIComplicationProviderService.kt
@@ -11,201 +11,239 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package com.google.wear.whereami.complication
 
-package com.google.wear.whereami.complication;
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Icon
+import android.location.Address
+import android.location.Location
+import android.location.LocationManager
+import android.os.RemoteException
+import android.text.TextUtils
+import android.util.Log
+import android.util.Pair
+import androidx.wear.complications.data.*
+import androidx.wear.complications.datasource.ComplicationDataSourceService
+import androidx.wear.complications.datasource.ComplicationRequest
+import com.google.android.gms.location.LocationRequest
+import com.google.wear.whereami.R
+import com.google.wear.whereami.WhereAmIActivity
+import com.patloew.rxlocation.RxLocation
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-import static android.location.LocationManager.GPS_PROVIDER;
-
-import android.Manifest;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Icon;
-import android.location.Address;
-import android.location.Location;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.wear.complications.data.*;
-
-import android.os.RemoteException;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Pair;
-import androidx.wear.complications.datasource.ComplicationDataSourceService;
-import androidx.wear.complications.datasource.ComplicationRequest;
-
-import com.google.android.gms.location.LocationRequest;
-import com.google.wear.whereami.R;
-import com.google.wear.whereami.WhereAmIActivity;
-import com.patloew.rxlocation.FusedLocation;
-import com.patloew.rxlocation.RxLocation;
-
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
-
-public class WhereAmIComplicationProviderService extends ComplicationDataSourceService {
-
-    private static final String TAG = "WhereAmIComplication";
-
-    private RxLocation rxLocation;
-    private final CompositeDisposable subscriptions = new CompositeDisposable();
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        rxLocation = new RxLocation(this);
+class WhereAmIComplicationProviderService : ComplicationDataSourceService() {
+    private var rxLocation: RxLocation? = null
+    private val subscriptions = CompositeDisposable()
+    override fun onCreate() {
+        super.onCreate()
+        rxLocation = RxLocation(this)
     }
 
-    @Override
-    public void onDestroy() {
-        subscriptions.dispose();
-        super.onDestroy();
+    override fun onDestroy() {
+        subscriptions.dispose()
+        super.onDestroy()
     }
 
-    @Override
-    public void onComplicationRequest(@NonNull ComplicationRequest complicationRequest, @NonNull ComplicationRequestListener complicationRequestListener) {
+    override fun onComplicationRequest(
+        request: ComplicationRequest,
+        listener: ComplicationRequestListener
+    ) {
         if (Log.isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, "onComplicationUpdate(): " + complicationRequest);
+            Log.d(TAG, "onComplicationUpdate(): $request")
         }
-
-        final Observable<Pair<Location, Address>> task;
-        if (checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            FusedLocation locationProvider = rxLocation.location();
-            task = locationProvider
-                    .isLocationAvailable()
+        val task: Observable<Pair<Location, Address>>
+        task =
+            if (checkCallingOrSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                val locationProvider = rxLocation!!.location()
+                locationProvider
+                    .isLocationAvailable
                     .subscribeOn(Schedulers.io())
-                    .flatMapObservable((hasLocation) -> hasLocation ?
-                            locationProvider.lastLocation().toObservable() :
-                            locationProvider.updates(createLocationRequest()))
-                    .flatMapMaybe((location) -> rxLocation.geocoding()
+                    .flatMapObservable { hasLocation: Boolean ->
+                        if (hasLocation) locationProvider.lastLocation()
+                            .toObservable() else locationProvider.updates(
+                            createLocationRequest()
+                        )
+                    }
+                    .flatMapMaybe { location: Location ->
+                        rxLocation!!.geocoding()
                             .fromLocation(location)
-                            .map(address -> Pair.create(location, address)));
-        } else {
-            task = Observable.error(new SecurityException("No location permission!"));
-        }
-
+                            .map { address: Address -> Pair.create(location, address) }
+                    }
+            } else {
+                Observable.error(SecurityException("No location permission!"))
+            }
         subscriptions.add(
             task
-                .subscribe(
-                        // onNext
-                        (locationAddressPair -> updateComplication(complicationRequest, complicationRequestListener, locationAddressPair.first, locationAddressPair.second)),
-                        // onError
-                        (error) -> {
-                            Log.w(TAG, "Error retrieving location", error);
-                            updateComplication(complicationRequest, complicationRequestListener, null, null);
-                        }
-                )
-        );
-    }
-
-    @Nullable
-    @Override
-    public ComplicationData getPreviewData(@NonNull ComplicationType complicationType) {
-        Location location = new Location(GPS_PROVIDER);
-        location.setLongitude(0.0);
-        location.setLatitude(0.0);
-
-        Address address = new Address(Locale.ENGLISH);
-        address.setCountryName("Null Island");
-
-        switch (complicationType) {
-            case SHORT_TEXT:
-                return new ShortTextComplicationData.Builder(getTimeAgo(location), getFullDescription(location, address))
-                                .setMonochromaticImage(new MonochromaticImage.Builder(Icon.createWithResource(this, R.drawable.ic_my_location)).build())
-                                .setTapAction(getTapAction())
-                                .build();
-            case LONG_TEXT:
-                return new LongTextComplicationData.Builder(getAddressDescriptionText(this, address), getFullDescription(location, address))
-                                .setTitle(getTimeAgo(location))
-                                .setMonochromaticImage(new MonochromaticImage.Builder(Icon.createWithResource(this, R.drawable.ic_my_location)).build())
-                                .setTapAction(getTapAction())
-                                .build();
-            default:
-                throw new IllegalArgumentException("Unexpected complication type " + complicationType);
-        }
-    }
-
-    private void updateComplication(ComplicationRequest complicationRequest, ComplicationRequestListener complicationRequestListener, Location location, Address address) throws RemoteException {
-        if (Log.isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, "Address: " + address);
-        }
-
-        ComplicationData complicationData = null;
-        switch (complicationRequest.getComplicationType()) {
-            case SHORT_TEXT:
-                complicationData =
-                        new ShortTextComplicationData.Builder(getTimeAgo(location), getFullDescription(location, address))
-                                .setMonochromaticImage(new MonochromaticImage.Builder(Icon.createWithResource(this, R.drawable.ic_my_location)).build())
-                                .setTapAction(getTapAction())
-                                .build();
-                break;
-            case LONG_TEXT:
-                complicationData =
-                        new LongTextComplicationData.Builder(getAddressDescriptionText(this, address), getFullDescription(location, address))
-                                .setTitle(getTimeAgo(location))
-                                .setMonochromaticImage(new MonochromaticImage.Builder(Icon.createWithResource(this, R.drawable.ic_my_location)).build())
-                                .setTapAction(getTapAction())
-                                .build();
-                break;
-            default:
-                if (Log.isLoggable(TAG, Log.WARN)) {
-                    Log.w(TAG, "Unexpected complication type " + complicationRequest);
+                .subscribe( // onNext
+                    { locationAddressPair: Pair<Location, Address> ->
+                        updateComplication(
+                            request,
+                            listener,
+                            locationAddressPair.first,
+                            locationAddressPair.second
+                        )
+                    }
+                )  // onError
+                { error: Throwable? ->
+                    Log.w(TAG, "Error retrieving location", error)
+                    updateComplication(request, listener, null, null)
                 }
+        )
+    }
+
+    override fun getPreviewData(type: ComplicationType): ComplicationData? {
+        val location = Location(LocationManager.GPS_PROVIDER)
+        location.longitude = 0.0
+        location.latitude = 0.0
+        val address = Address(Locale.ENGLISH)
+        address.countryName = "Null Island"
+        return when (type) {
+            ComplicationType.SHORT_TEXT -> ShortTextComplicationData.Builder(
+                getTimeAgo(location),
+                getFullDescription(location, address)
+            )
+                .setMonochromaticImage(
+                    MonochromaticImage.Builder(
+                        Icon.createWithResource(
+                            this,
+                            R.drawable.ic_my_location
+                        )
+                    ).build()
+                )
+                .setTapAction(tapAction)
+                .build()
+            ComplicationType.LONG_TEXT -> LongTextComplicationData.Builder(
+                getAddressDescriptionText(this, address),
+                getFullDescription(location, address)
+            )
+                .setTitle(getTimeAgo(location))
+                .setMonochromaticImage(
+                    MonochromaticImage.Builder(
+                        Icon.createWithResource(
+                            this,
+                            R.drawable.ic_my_location
+                        )
+                    ).build()
+                )
+                .setTapAction(tapAction)
+                .build()
+            else -> throw IllegalArgumentException("Unexpected complication type $type")
+        }
+    }
+
+    @Throws(RemoteException::class)
+    private fun updateComplication(
+        complicationRequest: ComplicationRequest,
+        complicationRequestListener: ComplicationRequestListener,
+        location: Location?,
+        address: Address?
+    ) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "Address: $address")
+        }
+        var complicationData: ComplicationData? = null
+        when (complicationRequest.complicationType) {
+            ComplicationType.SHORT_TEXT -> complicationData =
+                ShortTextComplicationData.Builder(
+                    getTimeAgo(location),
+                    getFullDescription(location, address)
+                )
+                    .setMonochromaticImage(
+                        MonochromaticImage.Builder(
+                            Icon.createWithResource(
+                                this,
+                                R.drawable.ic_my_location
+                            )
+                        ).build()
+                    )
+                    .setTapAction(tapAction)
+                    .build()
+            ComplicationType.LONG_TEXT -> complicationData = LongTextComplicationData.Builder(
+                getAddressDescriptionText(this, address),
+                getFullDescription(location, address)
+            )
+                .setTitle(getTimeAgo(location))
+                .setMonochromaticImage(
+                    MonochromaticImage.Builder(
+                        Icon.createWithResource(
+                            this,
+                            R.drawable.ic_my_location
+                        )
+                    ).build()
+                )
+                .setTapAction(tapAction)
+                .build()
+            else -> if (Log.isLoggable(TAG, Log.WARN)) {
+                Log.w(TAG, "Unexpected complication type $complicationRequest")
+            }
         }
 
         // If no data is sent, we still need to inform the ComplicationManager, so
         // the update job can finish and the wake lock isn't held any longer.
-        complicationRequestListener.onComplicationData(complicationData);
+        complicationRequestListener.onComplicationData(complicationData)
     }
 
-    private PendingIntent getTapAction() {
-        Intent intent = new Intent(this, WhereAmIActivity.class)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+    private val tapAction: PendingIntent
+        get() {
+            val intent = Intent(this, WhereAmIActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            return PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+
+    private fun getFullDescription(location: Location?, address: Address?): ComplicationText {
+        return if (location == null || address == null) PlainComplicationText.Builder(getString(R.string.no_location))
+            .build() else getTimeAgo(
+            location.time
+        )
+            .build()
     }
 
-    private ComplicationText getFullDescription(Location location, Address address) {
-        if (location == null || address == null) return new PlainComplicationText.Builder(getString(R.string.no_location)).build();
-
-        return getTimeAgo(location.getTime())
-                .build();
+    private fun getTimeAgo(location: Location?): ComplicationText {
+        return if (location == null) PlainComplicationText.Builder("--").build() else getTimeAgo(
+            location.time
+        ).build()
     }
 
-    public static ComplicationText getAddressDescriptionText(Context context, Address address) {
-        return new PlainComplicationText.Builder(getAddressDescription(context, address)).build();
+    private fun getTimeAgo(fromTime: Long): TimeDifferenceComplicationText.Builder {
+        return TimeDifferenceComplicationText.Builder(
+            TimeDifferenceStyle.SHORT_SINGLE_UNIT,
+            CountUpTimeReference(fromTime)
+        )
+            .setMinimumTimeUnit(TimeUnit.MINUTES)
+            .setDisplayAsNow(true)
     }
 
-    public static String getAddressDescription(Context context, Address address) {
-        if (address == null) return context.getString(R.string.no_location);
-        String subThoroughfare = address.getSubThoroughfare();
-        String thoroughfare = address.getThoroughfare();
-        if (thoroughfare == null) return address.getCountryName();
-        return (TextUtils.isEmpty(subThoroughfare) ? "" : subThoroughfare +  " ") + thoroughfare;
-    }
+    companion object {
+        private const val TAG = "WhereAmIComplication"
+        fun getAddressDescriptionText(context: Context, address: Address?): ComplicationText {
+            return PlainComplicationText.Builder(getAddressDescription(context, address)).build()
+        }
 
+        fun getAddressDescription(context: Context, address: Address?): String {
+            if (address == null) return context.getString(R.string.no_location)
+            val subThoroughfare = address.subThoroughfare
+            val thoroughfare = address.thoroughfare ?: return address.countryName
+            return (if (TextUtils.isEmpty(subThoroughfare)) "" else "$subThoroughfare ") + thoroughfare
+        }
 
-    private ComplicationText getTimeAgo(Location location) {
-        if (location == null) return new PlainComplicationText.Builder("--").build();
-        return getTimeAgo(location.getTime()).build();
-    }
-
-    private TimeDifferenceComplicationText.Builder getTimeAgo(long fromTime) {
-        return new TimeDifferenceComplicationText.Builder(TimeDifferenceStyle.SHORT_SINGLE_UNIT, new CountUpTimeReference(fromTime))
-                .setMinimumTimeUnit(TimeUnit.MINUTES)
-                .setDisplayAsNow(true);
-    }
-
-    public static LocationRequest createLocationRequest() {
-        return LocationRequest.create()
+        fun createLocationRequest(): LocationRequest {
+            return LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
                 .setNumUpdates(1)
-                .setExpirationDuration(TimeUnit.SECONDS.toMillis(30));
+                .setExpirationDuration(TimeUnit.SECONDS.toMillis(30))
+        }
     }
 }
